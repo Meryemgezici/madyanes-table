6edfc9fff1c1ae1c8284bdbe494dc5fbdaef47ed
MeryemGezici <mgezici@lojiper.com>
2024-06-10 13:42:58 +0300

    Table responsive

--- utils/tableColumns.js
+++ utils/tableColumns.js
@@ -14,12 +14,12 @@ export const stockedAccountTableCol = [
 ];
 // Cari ekstre tablosu sütunları
 export const accountTableCol = [
-  { key: "date", label: "Tarih" },
-  { key: "transactionType", label: "İşlem Türü" },
-  { key: "dueDate", label: "Vadesi" },
-  { key: "description1", label: "Açıklama 1" },
-  { key: "description", label: "Açıklama" },
-  { key: "debit", label: "Borç" },
-  { key: "credit", label: "Alacak" },
-  { key: "balance", label: "Bakiye" },
+  { key: "date", label: "Tarih", responsiveClass: "" },
+  { key: "transactionType", label: "İşlem Türü", responsiveClass: "" },
+  { key: "dueDate", label: "Vadesi", responsiveClass: "" },
+  { key: "description1", label: "Açıklama 1", responsiveClass: "  md:hidden  sm:hidden hidden lg:table-cell" },
+  { key: "description", label: "Açıklama", responsiveClass: "hidden lg:table-cell" },
+  { key: "debit", label: "Borç", responsiveClass: "hidden md:table-cell" },
+  { key: "credit", label: "Alacak", responsiveClass: "hidden md:table-cell " },
+  { key: "balance", label: "Bakiye", responsiveClass: "hidden md:table-cell" },
 ];
